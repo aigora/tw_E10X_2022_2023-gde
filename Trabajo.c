bb8e0fb2a61630bdc3fb9729fac5225920afda71
anasortega <130755382+anasortega@users.noreply.github.com>
2023-05-03 12:59:27 +0200

    Update Trabajo.c

--- Trabajo.c
+++ Trabajo.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+ {
+ 	int menu1, energia;
+ 	printf ("Que quiere consultar?\n -Datos de una sola energia(1)\n -Datos de varias energias(2)\n");
+ 	scanf ("%c", &menu1);
+ 	switch (menu1)
+ 		{
+ 			case 1:
+ 			{
+ 				printf ("Introduzca la energia que quiere consultar.\n");
+ 				printf ("Hidraulica(1)\n");
+ 				printf ("Turbinacion bombeo(2)\n");
+ 				printf ("Nuclear(3)\n");
+ 				printf ("Carbon(4)\n");
+ 				printf ("Fuel y gas (5)\n");
+ 				printf ("Motores diesel(6)\n");
+ 				printf ("Turbina de gas(7)\n");
+ 				printf ("Turbina de vapor(8)\n");
+ 				printf ("Ciclo combinado(9)\n");
+ 				printf ("Hidroeolica(10)\n");
+ 				printf ("Eolica(11)\n");
+ 				printf ("Solar fotovoltaica(12)\n");
+ 				printf ("Solar termica(13)\n");
+ 				printf ("Otras renovables(14)\n");
+ 				printf ("Cogeneracion(15)\n");
+ 				printf ("Residuos no renovables(16)\n");
+ 				printf ("Residuos renovables(17)\n");
+ 				printf ("Generacion total(18)\n");
+ 				scanf ("%i", &energia);
+ 				break;
+			}
+		}
+ 	
+ 	return 0;
+ }
