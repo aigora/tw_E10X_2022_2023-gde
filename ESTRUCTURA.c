0f4b396fa221f1fadb84e864befe5804365eaa3e
fati-ra <131274993+fati-ra@users.noreply.github.com>
2023-05-19 10:00:29 +0200

    Update ESTRUCTURA.c

--- ESTRUCTURA.c
+++ ESTRUCTURA.c
@@ -10,66 +10,77 @@ typedef struct {
 
 int main()
  {
- 	int i,ncomas;
- 	//int menu1, energia, energias;
+ 	int i,j;
+ 	int ncomas=0,nenergias=0,accion=0;
+ 	int energias [18];
+ 	int menu1, energia;
  	setlocale(LC_CTYPE,"spanish");
  	
  	Datos filas[30];
  	FILE *datos;
- 	if((datos = fopen("generacion_por_tecnologias_21_22.csv","r"))==NULL)
+ 	if((datos = fopen("generacion_por_tecnologias_21_22_puntos_simplificado.csv","r"))==NULL)
 	{
 
 	   printf("Se ha producido un error en la apertura del fichero de lectura");
 	   return -1;
     } else printf("Se ha abierto el fichero correctamente\n");
-    for(i=0;i<25;i++)//HASTA Y DESDE QUE FILA GUARDO
+    for(i=0;i<24;i++)//HASTA Y DESDE QUE FILA GUARDO
 	{
 	fgets(filas[i].filasenteras,sizeof(filas[i].filasenteras),datos);//fgets COJE SALTOS DE LINEA
 																  
-	filas[i].filasenteras[strlen(filas[i].filasenteras)-1]=','; //ASIGNO ';' EN ESA POSICION 
+	filas[i].filasenteras[strlen(filas[i].filasenteras)-1]=','; //ASIGNO ',' EN ESA POSICION 
 																//Y ELIMINO EL SALTO DE LINEA
-	filas[i].filasenteras[strlen(filas[i].filasenteras)-0]='\0';	
+	filas[i].filasenteras[strlen(filas[i].filasenteras)]='\0';	
 																//CIERRO CADENA														
     }
-    for(i=0;i<23;i++)
-	{//HASTA Y DESDE QUE FILA IMPRIMO
+    /*for(i=0;i<24;i++)
+	{
 	  	printf ("%s\n",filas[i].filasenteras);
-    }
-    for(ncomas=0,i=0;ncomas<2;i++)//AQUI DA FALLO	
-    {
-    	if(filas[i]  ==',')comas++;
-			printf("este es i: %i  y este es numcomas: %i\n",i,ncomas); //asi vemos en que posicion esta i
+    }*/
+    
+   
+  /*  for(j=0;filas[0].filasenteras[j]!='\0';j++){
+		  printf("%c",filas[0].filasenteras[j]);
+    	
+    	if (filas[0].filasenteras[j]==',') ncomas++;
+    	//printf("%i",ncomas);
+    //}	
 	}
-
-    
-    
- /*	switch (menu1)
- 		{
- 			case 1:
- 			{
- 				printf ("Introduzca la energia que quiere consultar.\n");
- 				printf ("Hidraulica(1)\n");
- 				printf ("Turbinacion bombeo(2)\n");
- 				printf ("Nuclear(3)\n");
- 				printf ("Carbon(4)\n");
- 				printf ("Fuel y gas (5)\n");
- 				printf ("Motores diesel(6)\n");
- 				printf ("Turbina de gas(7)\n");
- 				printf ("Turbina de vapor(8)\n");
- 				printf ("Ciclo combinado(9)\n");
- 				printf ("Hidroeolica(10)\n");
- 				printf ("Eolica(11)\n");
- 				printf ("Solar fotovoltaica(12)\n");
- 				printf ("Solar termica(13)\n");
- 				printf ("Otras renovables(14)\n");
- 				printf ("Cogeneracion(15)\n");
- 				printf ("Residuos no renovables(16)\n");
- 				printf ("Residuos renovables(17)\n");
- 				printf ("Generacion total(18)\n");
- 				scanf ("%i", &energia);
- 				break;
-			}
-		}*/
+	printf("\n%i",ncomas);*/
+	
+	printf("Quiere consultar (0) o añadir datos (1)\n");//Control de datos
+	scanf("%i",&accion);
+	if (accion==0)
+	{
+		printf("¿Cuantas energías quiere consultar? (introduzca un entero positivo)\n");
+    	scanf("%i",&nenergias);
+    	
+    printf ("Introduzca la energia/as que quiere consultar.\n");
+ 	printf ("Hidraulica(1)\n");
+ 	printf ("Turbinacion bombeo(2)\n");
+ 	printf ("Nuclear(3)\n");
+ 	printf ("Carbon(4)\n");
+ 	printf ("Fuel y gas (5)\n");
+ 	printf ("Motores diesel(6)\n");
+ 	printf ("Turbina de gas(7)\n");
+ 	printf ("Turbina de vapor(8)\n");
+ 	printf ("Ciclo combinado(9)\n");
+ 	printf ("Hidroeolica(10)\n");
+ 	printf ("Eolica(11)\n");
+ 	printf ("Solar fotovoltaica(12)\n");
+ 	printf ("Solar termica(13)\n");
+ 	printf ("Otras renovables(14)\n");
+ 	printf ("Cogeneracion(15)\n");
+ 	printf ("Residuos no renovables(16)\n");
+ 	printf ("Residuos renovables(17)\n");
+ 	printf ("Generacion total(18)\n");
+ 	
+ 	  for (i=0;i<nenergias;i++)//Control de datos
+ 	  {
+ 		scanf("%i",&energias[i]);
+	  }
+    }
+ 
  	
  	return 0;
  }
